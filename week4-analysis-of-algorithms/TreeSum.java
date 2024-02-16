4170a8ce50ed023cf1853bf9b1f4313db8a686a1
Jiali Zhang <zhangjiali86@gmail.com>
2024-02-16 23:53:23 +0100

    BinarySearch+ faster treesum

--- week4-analysis-of-algorithms/TreeSum.java
+++ week4-analysis-of-algorithms/TreeSum.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.Stopwatch;
@@ -16,6 +18,29 @@ public class TreeSum {
             }
         }
         return count;
+    }
+
+    public static int countFaster(int[] a) {
+        int n = a.length;
+        Arrays.sort(a);
+        if (containsDuplicates(a))
+            throw new IllegalArgumentException("array contains duplicate integers");
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int k = Arrays.binarySearch(a, -(a[i] + a[j]));
+                if (k > j)
+                    count++;
+            }
+        }
+        return count;
+    }
+
+    private static boolean containsDuplicates(int[] a) {
+        for (int i = 1; i < a.length; i++)
+            if (a[i] == a[i - 1])
+                return true;
+        return false;
     }
 
     public static void main(String[] args) {
